1405e880dd0f498966019eb4a52efaeac6dae5ba
wesley-nunes <wesnmonteiro@gmail.com>
2023-09-08 11:28:02 -0300

    refactor: Update the rollup config file.

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
+import terser from "@rollup/plugin-terser";
 import scss from "rollup-plugin-scss";
+
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
   input: "./src/App.js",
@@ -6,7 +9,15 @@ export default {
     {
       file: "dist/jsBundle.js",
       format: "iife",
+      sourcemap: true,
     },
   ],
-  plugins: [scss({ fileName: "cssBundle.css" })],
+  plugins: [
+    production
+      ? [
+          scss({ fileName: "cssBundle.css", outputStyle: "compressed" }),
+          terser(),
+        ]
+      : scss({ fileName: "cssBundle.css" }),
+  ],
 };
