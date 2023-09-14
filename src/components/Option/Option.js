5862dc0ed5810b4b9170433177a3a5e0dc94734f
wesley-nunes <wesnmonteiro@gmail.com>
2023-09-14 12:08:20 -0300

    feat: Create the option component

---
+++ src/components/Option/Option.js
@@ -0,0 +1,30 @@
+import "./Option.Style.css";
+/**
+ *
+ * @param {string} imgSrc - The src of the img
+ * @param {string} name - The name of the plan
+ * @param {Object} price - The month price and year price of the plan.
+ * @param {number} price.month - The month price.
+ * @param {number} price.year - The year price.
+ * @returns {HTMLOptionElement}
+ */
+function OptionComponent(imgSrc, name, price) {
+  const template = document.querySelector("#option-component");
+  const clone = template.content.cloneNode(true);
+  const img = clone.querySelector("img");
+  const optionName = clone.querySelector(".option-component__name");
+  const optionPrice = clone.querySelector(".option-component__price");
+  const extra = clone.querySelector(".option-component__extra");
+
+  clone.children[0].id = name;
+  img.src = imgSrc;
+  optionName.innerText = name;
+  // optionPrice.dataset.monthPrice = price.month;
+  // optionPrice.dataset.monthPrice = price.month;
+  optionPrice.innerText = `$${price.month}/mo`;
+  // extra.innerText = "2 months free";
+
+  return clone.children[0];
+}
+
+export default OptionComponent;
